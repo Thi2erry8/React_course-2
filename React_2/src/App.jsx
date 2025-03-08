f32b97a710c0acc0644bc5244550517faffb15a6
Thi2erry8 <jonathanaugustin056@gmail.com>
2025-03-08 13:17:07 -0500

    ternary operator

--- React_2/src/App.jsx
+++ React_2/src/App.jsx
@@ -1,21 +1,20 @@
 import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
+
 import './App.css'
 
-function App() {
-  const names = ["charlie","bob","alice","danielle"];
-  
+function App() {  
+   
+  const isGointOut = false ;
+  let answer = isGointOut === true ? "Yes" : "No" ;
+
 
   return (
-    <>
-       <ul> 
-        <br/>
-            {names.map((name)=>{
-               return <>  <li>{name}</li> <br /> </>
-            })}
-       </ul>
-    </>
+    <main> 
+            <h1> Do i feel like doing out tonight ?</h1>
+            <button className=''>{answer}</button>
+    </main>
   )
 }
 
---
+++ React_2/src/assets/components/header.jsx
@@ -0,0 +1,8 @@
+import './header.css' ;
+
+function Header() {
+     return <header>
+                     <p className='logo_text'>Chef Claude</p>
+            </header> 
+}
+export default Header
